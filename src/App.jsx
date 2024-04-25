ce0315920afc567b18c300c7d327b0d133cc6ccc
Your Name <you@example.com>
2024-04-25 13:06:39 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,76 @@
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import LoginSignup from "./Pages/LoginSignup";
 import Cart from "./Pages/Cart";
-import Footer from "./Components/Footer/Footer";
 import men_banner from "./Components/Assets/banner_mens.png";
 import women_banner from "./Components/Assets/banner_women.png";
 import kid_banner from "./Components/Assets/banner_kids.png";
 import ShopContextProvider from "./Context/ShopContext";
+import Admin from "./Pages/Pages/Admin/Admin";
+import AddProduct from "./Pages/Components/AddProduct/AddProduct";
+import ListProduct from "./Pages/Components/ListProduct/ListProduct";
 
-function App() {
-  return (
-    
-      <ShopContextProvider>
-        <BrowserRouter>
+const App = () => { 
 
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route
-              path="/mens"
-              element={<ShopCategory banner={men_banner} category="men" />}
-            />
-            <Route
-              path="/womens"
-              element={<ShopCategory banner={women_banner} category="women" />}
-            />
-            <Route
-              path="/kids"
-              element={<ShopCategory banner={kid_banner} category="kid" />}
-            />
-            <Route path="product" element={<Product />}>
-              <Route path=":productId" element={<Product />}></Route>
-            </Route>
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<LoginSignup />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </ShopContextProvider>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Shop />,
+  },
+  {
+    path: "/mens",
+    element: <ShopCategory banner={men_banner} category="men" />,
+  },
+  {
+    path: "/womens",
+    element: <ShopCategory banner={women_banner} category="women" />,
+  },
+  {
+    path: "/kids",
+    element: <ShopCategory banner={kid_banner} category="kid" />,
+  },
+  {
+    path: "/product",
+    element: <Product />,
+  },
+  {
+    path: "/product/:productId",
+    element: <Product />,
+  },
+  {
+    path: "/product/:productId",
+    element: <Product />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/login",
+    element: <LoginSignup />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+  {
+    path: "/addproduct",
+    element: <AddProduct />,
+  },
+  {
+    path: "/listproduct",
+    element: <ListProduct />,
+  },
+]);
 
-
-  );
+return (
+  <ShopContextProvider>
+     <RouterProvider router={router} />
+  </ShopContextProvider>
+)
 }
 
 export default App;
--- src/Pages/Components/ListProduct/ListProduct.jsx
+++ src/Pages/Components/ListProduct/ListProduct.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import './ListProduct.css';
-
 
 const ListProduct = () => {
   return (
     <div className='list-product'>
-
+      <h1>product list</h1>
     </div>
   )
 }
